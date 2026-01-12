858e671814b59a99745a913ab29f76a2be30bdea
egsmartin <egsmartin@gmail.com>
2026-01-12 08:35:47 +0100

    .

--- Learning Go/ch08 generics/impossible/main.go
+++ Learning Go/ch08 generics/impossible/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// se implementara esta interface si incluimos la definición de String() y el se trata de un tipo int, o un tipo derivado de int8
 type ImpossiblePrintableInt interface {
 	int | ~int8
 	String() string
@@ -11,12 +12,14 @@ type ImpossibleStruct[T ImpossiblePrintableInt] struct {
 	val T
 }
 
+// MyInt no implementa la interface ImpossiblePrintableInt porque es un tipo derivado de int y aunque tiene el método String(), no es int ni un derivado de int8 (si hubieramos definido ~int en la interface si lo hubiera implementado)
 type MyInt int
 
 func (mi MyInt) String() string {
 	return fmt.Sprint(mi)
 }
 
+// MyInt8 implementa la interface ImpossiblePrintableInt porque es un tipo derivado de int8 y tiene el método String()
 type MyInt8 int8
 
 func (mi MyInt8) String() string {
